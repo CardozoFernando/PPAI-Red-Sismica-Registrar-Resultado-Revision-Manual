5c1975e839278b3a0c54dfee141e282c1f2c2e3a
Fernando <89064@sistemas.frc.utn.edu.ar>
2025-05-25 15:18:40 -0300

    agregndo algunos atributos

--- PPAI Red Sismica Registrar Resultado Revision Manual/Entidades/EventoSismico.cs
+++ PPAI Red Sismica Registrar Resultado Revision Manual/Entidades/EventoSismico.cs
@@ -4,15 +4,31 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
+
 namespace PPAI_Red_Sismica_Registrar_Resultado_Revision_Manual.Entidades
 {
     public class EventoSismico
     {
         private DateTime fechaHoraFin;
-
-        public EventoSismico(DateTime fechaHoraFin)
+        private List<EventoSismico> todosEventosSismicos = new List<EventoSismico>();
+        private List<Estado> todosEstados = new List<Estado>();
+        private EventoSismico eventoSismicoSeleccionado;
+        private Pantalla pantallaRegRevisionManual;
+            /*
+            - todosEstados
+            -pantallaRegRevisionManual
+            - todosEventosSismicos
+            - eventoSismicoSeleccionado
+            - datosAlcanceClasificacionOrigen
+            - datosEvento
+            - seleccionAccion
+            - sesion
+            - aSLogueado
+            */
+        public EventoSismico(DateTime fechaHoraFinal) //constructor
         {
-            this.FechaHoraFin = fechaHoraFin;
+            fechaHoraFin = fechaHoraFinal;
+            
         }
 
         public DateTime FechaHoraFin { get => fechaHoraFin; set => fechaHoraFin = value; }
