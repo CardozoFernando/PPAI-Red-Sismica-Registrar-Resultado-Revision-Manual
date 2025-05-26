d187f3bf7f97534813ec3427046e525b0b894ab6
Cardozo Fernando <166088063+CardozoFernando@users.noreply.github.com>
2025-05-26 16:58:42 -0300

    Create EstacionSismologica.cs

---
+++ PPAI Red Sismica Registrar Resultado Revision Manual/Entidades/EstacionSismologica.cs
@@ -0,0 +1,16 @@
+namespace PPAI_Red_Sismica_Registrar_Resultado_Revision_Manual.Entidades
+{
+    public class EstacionSismologica
+    {
+        private string nombre;
+
+        public EstacionSismologica(string nombre)
+        {
+            this.nombre = nombre;
+        }
+
+        public string Nombre { get => nombre; set => nombre = value; }
+    }
+
+    
+}
