672a27ee5d0ecf162b83c152c24f169eebdfd788
Fernando <89064@sistemas.frc.utn.edu.ar>
2025-05-27 11:08:18 -0300

    agrego DetalleMuestra y metodos

--- PPAI Red Sismica Registrar Resultado Revision Manual/Entidades/DetalleMuestra.cs
+++ PPAI Red Sismica Registrar Resultado Revision Manual/Entidades/DetalleMuestra.cs
@@ -17,5 +17,13 @@ namespace PPAI_Red_Sismica_Registrar_Resultado_Revision_Manual.Entidades
             this.tipoDeDato = tipoDeDato;
         }
 
+        public int Valor { get => valor; set => valor = value; }
+        public TipoDeDato TipoDeDato { get => tipoDeDato; set => tipoDeDato = value; }
+
+        public string getDatos() {
+            string denominacion = tipoDeDato.Denominacion;
+            string unidadDeMedida = tipoDeDato.UnidadDeMedida;
+            return denominacion + ": " + valor + " "  + unidadDeMedida; 
+        }
     }
 }
